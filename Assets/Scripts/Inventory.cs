0c9cbc3563c6fdf2ffdf39ce2ec4639afbce9231
NoahSmo <90006474+NoahSmo@users.noreply.github.com>
2022-10-31 16:02:25 +0100

    Ajout du système d'inventaire

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private List<ItemData> content = new List<ItemData>();
+
+    [SerializeField] 
+    private GameObject inventoryPanel;
+
+    [SerializeField] 
+    private Transform inventorySlotsParent;
+
+    const int InventorySize = 24;
+
+    public void AddItem(ItemData item)
+    {
+        content.Add(item);
+        RefreshContent();
+    }
+
+    private void Start()
+    {
+        RefreshContent();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown( KeyCode.I))
+        {
+            inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+        }
+    }
+
+    public void CloseInventory()
+    {
+        inventoryPanel.SetActive(false);
+    }
+
+    private void RefreshContent()
+    {
+        for (int i = 0; i < content.Count; i++)
+        {
+            inventorySlotsParent.GetChild(i).GetChild(0).GetComponent<Image>().sprite = content[i].visual;
+        }
+    }
+
+    public bool IsFull()
+    {
+        return InventorySize == content.Count;
+    }
+}
---
+++ Assets/Scripts/PickupItem.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    [SerializeField]
+    private float pickupRange = 2.6f;
+
+    public PickupBehaviour playerPickupBehaviour;
+    
+    [SerializeField] 
+    private GameObject pickupText;
+
+    [SerializeField] 
+    private LayerMask layerMask;
+    void Update()
+    {
+        
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, pickupRange, layerMask))
+        {
+            if (hit.transform.CompareTag("Item"))
+            {
+                // Debug.Log("Item in front of us");
+                
+                pickupText.SetActive(true);
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    playerPickupBehaviour.DoPickup(hit.transform.gameObject.GetComponent<Item>());
+                }
+            }
+        }
+        else
+        {
+            pickupText.SetActive(false);
+        }
+    }
+}
